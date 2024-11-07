60f59d3a656d5cec7c1ff29db434bf317d0a65ff
itohara <ito.k23018@gmail.com>
2024-11-07 19:26:29 +0530

    sqlite to pg

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.2].define(version: 2024_10_21_152122) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "quotes", force: :cascade do |t|
     t.text "content"
     t.integer "user_id"
