2500daee0fda948aff93ef76a1957a893e40b153
itohara <ito.k23018@gmail.com>
2024-11-10 17:01:04 +0530

    add:users controller remove:extra navlinks

--- test/integration/layout_site_test.rb
+++ test/integration/layout_site_test.rb
@@ -7,11 +7,12 @@ class LayoutSiteTest < ActionDispatch::IntegrationTest
 
   test "layout navlinks" do
     get root_path
-    assert_select "a[href=?]", "/"
+    assert_select "a[href=?]", "/", count: 1
     assert_select "a[href=?]", "/hello"
     assert_select "a[href=?]", "/help"
     assert_select "a[href=?]", "/contact"
     assert_select "a[href=?]", "/about"
+    assert_select "a[href=?]", "/signup"
 
     # TO CHECK THE RENDERED VIEW TEMPLATE
     assert_template "static_pages/home"
